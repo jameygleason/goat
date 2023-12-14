9b508a082c7b199eace9b32c7a51a180c8d07c67
jameygleason <james.p.gleason@gmail.com>
2023-12-14 14:50:01 -0500

    added push script

---
+++ .config/push.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+git remote set-url origin git@github.com:jameygleason/goat.git
+git push origin
+
+git remote set-url origin git@gitlab.com:jameygleason/goat.git
+git push origin
